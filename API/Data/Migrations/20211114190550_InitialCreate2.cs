308a015bc5cb78e81216d071bea80f0481a663ea
Andrem19 <7255591@gmail.com>
2021-11-14 20:48:31 +0000

    End of section 2

---
+++ API/Data/Migrations/20211114190550_InitialCreate2.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class InitialCreate2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "description",
+                table: "Products",
+                newName: "Description");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PictureUrl",
+                table: "Products",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PictureUrl",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Products",
+                newName: "description");
+        }
+    }
+}
